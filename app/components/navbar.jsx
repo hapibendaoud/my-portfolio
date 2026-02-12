ff6433bcd98dd1f74af6e30dbc781e653f5c9f93
hapibendaoud <hapibendaoud@gmail.com>
2026-02-12 20:14:20 +0100

    Initial commit

---
+++ app/components/navbar.jsx
@@ -0,0 +1,52 @@
+"use client"
+
+import Link from "next/link";
+import Image from "next/image";
+import "../globals.css";
+import { useState } from "react";
+
+
+export default function Navbar() {
+    const links = [
+        {
+            title: "Home",
+            href: "/"
+        },
+        {
+            title: "About",
+            href: "/about"
+        },
+        {
+            title: "Projects",
+            href: "/projects"
+        },
+        {
+            title: "Blogs",
+            href: "/blogs"
+        },
+    ]
+
+    const [hovered, setHovered] = useState(false)
+
+    return(
+        <nav className="w-full h-fit flex items-center justify-center pt-3 fixed z-50 ">
+            <div className="md:max-w-3xl h-fit flex items-center justify-center md:gap-10 gap-4 rounded-4xl  px-2.5 py-2 border border-gray-200 dark:border-none bg-white dark:bg-black">
+                <div className="rounded-4xl overflow-hidden hover:brightness-50 cursor-pointer">
+                    <Image src={"/said.jpeg"} alt="saidDev" width={40} height={40}></Image>
+                </div>
+                
+                <div className="md:flex flex-wrap items-center justify-center gap-5 text-sm dark:text-white text-gray-600 hidden md:visible">
+                    {links.map((link, index) => (
+                        <Link key={link.href} href={link.href}>{link.title}
+                        </Link>
+                    ))}
+                </div>
+                <div className="flex flex-wrap items-center justify-center text-white md:hidden visible">
+                    <span>Available for work </span>
+                </div>
+                <Link href={"#"} className="md:flex md:visible hidden px-8 py-2 rounded-4xl text-sm dark:bg-white bg-[#303030] dark:text-black text-white hover:bg-[#5E67E6] dark:hover:bg-[#D0FF71]">Contact</Link>
+                <Link href={"#"} className="flex md:hidden visible px-4.5 py-2 rounded-4xl bg-white dark:text-black text-white text-2xl ">=</Link>
+            </div>
+        </nav>
+    );
+}
---
+++ app/page.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main >
+        <div className="h-full  grid lg:grid-cols-3 grid-cols-1 pt-20 dark:bg-[#303030]">
+          <div className=" h-full w-full flex flex-col lg:items-end max-sm:items-center justify-center max-lg:pt-30 max-lg:pl-20">
+            <h3 className="font-bebas text-4xl lg:text-lg sm:text-sm tracking-[-0.03em] 
+            scale-y-210 font-extrabold lg:pb-3 pb-5 text-[#303030] dark:text-white"
+            >SAID AIT BENDAOUD</h3>
+            <h1 className="font-bebas text-4xl lg:text-6xl sm:text-5xl tracking-[-0.03em] 
+            scale-y-210 font-extrabold max-md:text-6xl text-[#303030] dark:text-white"
+            >FRONTEND</h1>
+          </div>
+          
+            <div className="h-full w-full flex items-center justify-center max-lg:pt-20 max-lg:px-3 relative">
+              <Image src={"/MAINSAID.PNG"} alt="said" width={500} height={500} 
+              className="lg:w-[85%] lg:h-[90%] max-lg:h-[90%] rounded-2xl"
+              ></Image>
+            <div className="w-30 h-30 absolute bottom-0 left-0 bggreen rounded-[50%] 
+            flex justify-center items-center text-4xl text-[#303030]">Hi</div>
+            </div>
+          <div className=" h-full w-full flex lg:flex-wrap items-center lg:justify-start max-lg:justify-end 
+          max-lg:pr-10 max-lg:pt-20 justify-center lg:items-center">
+            <h1 className="font-bebas text-4xl lg:text-6xl md:text-5xl tracking-[-0.03em] scale-y-210 
+            font-extrabold max-lg:text-5xl text-[#303030] dark:text-white lg:pt-4">DEVELOPER</h1>
+          </div>
+        </div>
+        <div className="h-100">
+          
+        </div>
+      </main>
+  );
+}
